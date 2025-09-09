7fcc3a451f47f7bdc6861b252141b9f60c50257d
27-CFIAD-BBustado <27-cfiad-bbustado@BAKAWAN-ADMINs-MacBook-Air.local>
2025-09-09 11:15:47 +0800

    refresh project

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App/App";
-import store from "./app/store";
+import store from "./App/store";
 import "./assets/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
