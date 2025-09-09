0f35b357841dac8fc301aff12b3573d595c91934
27-CFIAD-BBustado <27-cfiad-bbustado@BAKAWAN-ADMINs-MacBook-Air.local>
2025-09-09 13:07:57 +0800

    I add name and icons here

--- src/App/routes.jsx
+++ src/App/routes.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import { FaTachometerAlt, FaBox, FaClipboardList } from "react-icons/fa";
 import RecordsFeature from "../features/records";
 
 const routes = [
   {
     path: "/",
     element: <h1 className="text-2xl font-bold">Welcome Home</h1>,
+    name: "Dashboard",
+    icon: FaTachometerAlt,
   },
   {
     path: "/records",
     element: <RecordsFeature />,
+    name: "Records",
+    icon: FaClipboardList,
   },
 ];
 
